91cf81f2a17491efcec1ca70b69bad058d7c847f
GabrielRQueiroz <gabrielr.queiroz@gmail.com>
2024-04-25 02:10:36 -0300

    ✨ feat(Movies): Full CRUD

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,18 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+    # ordered
+    @movies = Movie.order('title ASC').all
   end
 
   def show
-    id = params[:id]
-    @movie = Movie.find(id)
+    begin
+      id = params[:id]
+      @movie = Movie.find(id)
+    rescue ActiveRecord::RecordNotFound
+      flash[:warning] = 'Movie not found.'
+      redirect_to movies_path
+    end
+
   end
 
   def new
@@ -13,7 +20,38 @@ class MoviesController < ApplicationController
   end
 
   def create
-    params.require(:movie).permit(:title, :rating, :release_date)
+    @movie = Movie.create!(permitted_params)
+
+    flash[:notice] = "#{@movie.title} was created successfully."
+
+    redirect_to movie_path(@movie)
   end
 
+  def edit
+    @movie = Movie.find params[:id]
+  end
+
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update!(permitted_params)
+
+    flash[:notice] = "#{@movie.title} was updated successfully."
+
+    redirect_to movie_path(@movie)
+  end
+
+  def destroy
+    @movie = Movie.find params[:id]
+    @movie.destroy
+
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+
+    redirect_to movies_path
+  end
+
+  private
+
+  def permitted_params
+    params.require(:movie).permit(:title, :rating, :release_date)
+  end
 end
